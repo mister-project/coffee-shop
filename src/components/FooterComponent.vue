091221021bfb9c85e7551671834e90959ac1f992
Yuriy Kulikov <kulikov-y@yandex.ru>
2025-01-21 19:27:35 +0300

    props в хедере и футере - поправил по проверке от Саши

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,70 +1,91 @@
 <template>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                    <ul class="footer d-flex flex-wrap">
-                        <li class=" footer__item">
-                            <router-link :to="links[0].link"><img :src="require(`@/assets/logo/${links[0].icon}`)"
-                                    :alt="links[0].icon"></router-link>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+          <ul class="footer d-flex flex-wrap">
+            <li class="footer__item">
+              <router-link :to="links[0].link"
+                ><img
+                  :src="require(`@/assets/logo/${links[0].icon}`)"
+                  :alt="links[0].icon"
+              /></router-link>
+            </li>
 
-                        </li>
+            <!-- <link-page
+              classLink="footer__item"
+              :text="links[1].text"
+              :link="links[1].link"
+            />
+            <link-page
+              classLink="footer__item"
+              :text="links[2].text"
+              :link="links[2].link"
+            />
+            <link-page
+              classLink="footer__item"
+              :text="links[3].text"
+              :link="links[3].link"
+            /> -->
 
-                        <link-page classLink="footer__item" :text="links[1].text" :link="links[1].link" />
-                        <link-page classLink="footer__item" :text="links[2].text" :link="links[2].link" />
-                        <link-page classLink="footer__item" :text="links[3].text" :link="links[3].link" />
-
-
-                        <!-- <li class="footer__item">
-                            
-                            <router-link :to="links[1].link">{{links[1].text}}</router-link>
-                        </li>
-                        <li class="footer__item">
-                          
-                            <router-link :to="links[2].link">{{links[2].text}}</router-link>
-                        </li>
-                        <li class="footer__item">
-                            <router-link :to="links[3].link">{{links[3].text}}</router-link>
-                        </li> -->
-                    </ul>
-                </div>
-            </div>
-            <img class="beanslogo" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
+            <nav-item
+              :link="links[1].link"
+              :text="links[1].text"
+              classLink="footer__item"
+            ></nav-item>
+            <nav-item
+              :link="links[2].link"
+              :text="links[2].text"
+              classLink="footer__item"
+            ></nav-item>
+            <nav-item
+              :link="links[3].link"
+              :text="links[3].text"
+              classLink="footer__item"
+            ></nav-item>
+          </ul>
         </div>
-    </footer>
+      </div>
+      <img
+        class="beanslogo"
+        src="@/assets/logo/Beans_logo_dark.svg"
+        alt="Beans logo"
+      />
+    </div>
+  </footer>
 </template>
 
 <script>
-import LinkPage from './LinkPage.vue';
-export default {
-    components: { LinkPage },
-    data() {
-        return {
-            links: [
-                {
-                    id: 0,
-                    // text: '',
-                    link: '/',
-                    icon: 'Logo_black.svg'
-                },
-                {
-                    id: 1,
-                    text: 'Our coffee',
-                    link: '/our-coffee',
-                },
-                {
-                    id: 2,
-                    text: 'For your pleasure',
-                    link: '/your-pleasure',
-                },
-                {
-                    id: 3,
-                    text: 'Contact us',
-                    link: '/contact-us',
-                },
-            ]
-        }
-    }
-}
+import NavItem from "@/components/NavItem.vue";
 
+export default {
+  components: { NavItem },
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          // text: '',
+          link: "/",
+          icon: "Logo_black.svg",
+        },
+        {
+          id: 1,
+          text: "Our coffee",
+          link: "/our-coffee",
+        },
+        {
+          id: 2,
+          text: "For your pleasure",
+          link: "/your-pleasure",
+        },
+        {
+          id: 3,
+          text: "Contact us",
+          link: "/contact-us",
+        },
+      ],
+    };
+  },
+};
 </script>
--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,50 +1,83 @@
 <template>
-    <header>
-        <ul class="header d-flex justify-content-center justify-content-md-start flex-wrap">
-            <li class="header__item">
-                <router-link :to="links[0].link">
-                    <img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon" />
-                </router-link>
-            </li>
+  <header>
+    <ul
+      class="header d-flex justify-content-center justify-content-md-start flex-wrap"
+    >
+      <li class="header__item">
+        <router-link :to="links[0].link">
+          <img
+            :src="require(`@/assets/logo/${links[0].icon}`)"
+            :alt="links[0].icon"
+          />
+        </router-link>
+      </li>
 
-            <link-page classLink="header__item" :text="links[1].text" :link="links[1].link" />
-            <link-page classLink="header__item" :text="links[2].text" :link="links[2].link" />
-            <link-page classLink="header__item" :text="links[3].text" :link="links[3].link" />
-        </ul>
-    </header>
+      <!-- <link-page
+        classLink="header__item"
+        :text="links[1].text"
+        :link="links[1].link"
+      />
+      <link-page
+        classLink="header__item"
+        :text="links[2].text"
+        :link="links[2].link"
+      />
+      <link-page
+        classLink="header__item"
+        :text="links[3].text"
+        :link="links[3].link"
+      /> -->
+
+      <nav-item
+        :link="links[1].link"
+        :text="links[1].text"
+        classLink="header__item"
+      ></nav-item>
+      <nav-item
+        :link="links[2].link"
+        :text="links[2].text"
+        classLink="header__item"
+      ></nav-item>
+      <nav-item
+        :link="links[3].link"
+        :text="links[3].text"
+        classLink="header__item"
+      ></nav-item>
+    </ul>
+  </header>
 </template>
 
 <script>
-import LinkPage from "./LinkPage.vue";
+import NavItem from "./NavItem.vue";
 
 export default {
-    components: { LinkPage },
-    data() {
-        return {
-            links: [
-                {
-                    id: 0,
-                    // text: '',
-                    link: "/",
-                    icon: "Logo.svg",
-                },
-                {
-                    id: 1,
-                    text: "Our coffee",
-                    link: "/our-coffee",
-                },
-                {
-                    id: 2,
-                    text: "For your pleasure",
-                    link: "/your-pleasure",
-                },
-                {
-                    id: 3,
-                    text: "Contact us",
-                    link: "/contact-us",
-                },
-            ],
-        };
-    },
+  components: { NavItem },
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          // text: '',
+          link: "/",
+          icon: "Logo.svg",
+        },
+        {
+          id: 1,
+          text: "Our coffee",
+          link: "/our-coffee",
+        },
+        {
+          id: 2,
+          text: "For your pleasure",
+          link: "/your-pleasure",
+        },
+        {
+          id: 3,
+          text: "Contact us",
+          link: "/contact-us",
+        },
+      ],
+    };
+  },
 };
 </script>
